a1a2d8e86befe31d6438c9c8ec1e56281a295a7c
Sameerahmadkhan <samerrkaan9@gmail.com>
2023-01-24 13:09:26 +0500

    navbar added

---
+++ src/components/FrontPage.js
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+// function FrontPage() {
+//   return (
+//     <div>
+//         <div className=''>
+//       <h1>halo World</h1>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default FrontPage
