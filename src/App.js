18d1a093874723153efeb30cf9f07f021c6eb60e
Sameerahmadkhan <samerrkaan9@gmail.com>
2023-01-28 15:52:22 +0500

    frontpage done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import FrontPage from './components/FrontPage';
+import About from './components/About';
 
 function App() {
   return (
     <div>
       <Header />
       <FrontPage />
+      <About />
     </div>
   );
 }
